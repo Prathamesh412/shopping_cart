3491294033c6cde01dbe9212d0e6a814012756ea
Prathamesh <prathprabhu@gmail.com>
2015-11-17 14:14:00 +0530

    This is changes

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
 //Card- template function
-var cart = function(name, desc, price){
-//  this.name = name;
-//  this.desc = desc;
-//  this.price = price;
+
+
+var cart = function(product){
+
     
-totalItems=items.length+1;
-    
-for(var i = 0; i< this.names.length;i++){
-        if (i=0){
-            price=price[1]
-        }
-         else{
-      price = price[i] + price [i + 1];
-         }
-    }
-    
+
 };
 
-var product=function(){
+totalPrice=0;
+
+var product=function(desc,name,price){
+   this.name = name;
+   this.desc = desc;
+   this.price = price;
+    
+    }
+};
+
+
+
+function addToCart(product)
+{
     this.desc = ['nice','warm','cool','cold','hot'];
     this.names = ['Fruit','Juice','Spades','Clubs'];
     this.price = ['12','23','34','40','80'];
-    var items = [];
+    
+    
+  var items = [];
     
      for(var j = 0; j< this.names.length;j++){
       var itemObject = new item(j+1, this.names[j], this.desc[j], this.price[j]);
       items.push(itemObject);
-    }
-};
+         
+       console.log(itemObject + "has been added to Cart");
 
-var myDeck = new product();
+}
 
-    div = document.createElement('div');
-    div.className = 'card';
-    div.innerHTML ='<span class="number">'+ myDeck[i].name +'</span><span class="suit"></span>';
-    document.body.appendChild(div);
+ function removeFromCart(index){
+  productName = cartObject.index.name;
+  cartObject.pop[index];
+  return productName + "is removed from Cart";
+}
